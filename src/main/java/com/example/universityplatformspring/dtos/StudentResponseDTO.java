a869b02e670aa41bca415c460ee62a46ecd52698
Sofiya Velcheva <sof.velcheva@gmail.com>
2023-03-11 11:28:38 +0200

    Add src/ file.

---
+++ src/main/java/com/example/universityplatformspring/dtos/StudentResponseDTO.java
@@ -0,0 +1,12 @@
+package com.example.universityplatformspring.dtos;
+
+import lombok.Data;
+
+@Data
+public class StudentResponseDTO {
+
+    private String id;
+    private String firstName;
+    private String lastName;
+
+}
